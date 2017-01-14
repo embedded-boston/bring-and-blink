682582b068d8eba1c51630968a7715ce12ae3046
Christopher J. Woodall <chris.j.woodall@gmail.com>
2017-01-14 15:50:17 -0500

    Updated publish presentations

--- scripts/publish-presentations.sh
+++ scripts/publish-presentations.sh
@@ -8,7 +8,11 @@ for lesson in $GIT_BASEDIR/lessons/*; do
     $MDTOSLIDES $lesson/README.md -i -o $GIT_BASEDIR/slides/$(basename $lesson).html --document-mode flag
 done
 
+mv slides /tmp/slides
 git checkout gh-pages
+rm -r $GIT_BASEDIR/slides
+mv /tmp/slides slides
+tree .
 git add $GIT_BASEDIR/slides
 git commit -m "Added slides"
 git push origin gh-pages
